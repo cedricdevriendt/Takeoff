386168e7a2a071c5f0c04fe29f38f25db31ac8fc
ceetn <blonkie@gmail.com>
2012-12-16 23:05:05 +0100

    Making all paths in the settings dynamic

--- zeppelin/settings.py
+++ zeppelin/settings.py
@@ -3,6 +3,8 @@
 import os.path
 import logging
 
+PROJECT_DIR = os.path.dirname(__file__)
+
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
@@ -48,12 +50,14 @@ USE_TZ = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
-MEDIA_ROOT = ''
+MEDIA_ROOT = os.path.join(os.path.dirname(__file__), '../takeoff/upload').replace('\\','/')
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
 # Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
-MEDIA_URL = ''
+MEDIA_URL = os.path.join(os.path.dirname(__file__), '../takeoff/upload').replace('\\','/')
+
+FILE_UPLOAD_PERMISSIONS = 0644
 
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
@@ -106,11 +110,7 @@ ROOT_URLCONF = 'zeppelin.urls'
 WSGI_APPLICATION = 'zeppelin.wsgi.application'
 
 TEMPLATE_DIRS = (
-	"C:/takeoff/takeoff/views",
-	#os.path.join(os.path.dirname(__file__), 'views/static').replace('\\','/'),
-    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
+	os.path.join(os.path.dirname(__file__), '../takeoff/views').replace('\\','/'),
 )
 
 INSTALLED_APPS = (
@@ -120,9 +120,7 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # Uncomment the next line to enable the admin:
     'django.contrib.admin',
-    # Uncomment the next line to enable admin documentation:
     'django.contrib.admindocs',
 	'takeoff',
 	'tastypie',
